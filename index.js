9e6a10642f3d0c5d1076c46565432cd37b6b6ccd
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-03 15:55:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+// Code your solutions in this file!
+const gifts =["teddy bear","drone","doll"];
+
+function wrapGift(gifts){
+    for (let i=0;i<gifts.length;i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger
+    }
+    return gifts 
+}
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+
+  wrapGift(gifts);
+
+  function writeCards(stringArray,eventName){
+      const stringLength = stringArray.length;
+      const newArray = []
+      for(let i=0;i<stringLength;i++){
+          newArray.push (`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`)
+          console.log(newArray);
+      }
+      return newArray;
+  }
+  
+  writeCards(["Guadalipe","Ollie","Aki"], "birthday");
+
+function countDown(initialNumber){
+    let i=initialNumber;
+    while (i=>0){
+        console.log(i);
+        i--;
+    }
+}
